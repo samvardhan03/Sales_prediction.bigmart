dc8620a9ad07898557e3a1d6a06f72ffca97bed5
Samvardhan Singh <104402477+samvardhan03@users.noreply.github.com>
2024-03-23 23:27:35 +0530

    Update sales_deploy.py

--- sales_deploy.py
+++ sales_deploy.py
@@ -36,36 +36,24 @@ def preprocess_input(data):
             m = Item_Visibility_Mean.loc[Item_Visibility_Mean['Item_Type_Category'] == cat]['Item_Visibility']
             data.at[i, 'Item_Visibility'] = m
 
-    if 'Outlet_Establishment_Year' in data.columns:
-        data['Operation_Years'] = 2013-data['Outlet_Establishment_Year']
-        data = data.drop(['Outlet_Establishment_Year'], axis=1)
-    else:
-        st.warning("Warning: 'Outlet_Establishment_Year' column not found in input data.")
+    data['Operation_Years'] = 2013-data['Outlet_Establishment_Year']
+    data=data.drop(['Item_Type', 'Outlet_Establishment_Year'], axis=1)
 
-    lb = LabelEncoder()
-    data['Outlet'] = lb.fit_transform(data['Outlet_Identifier'])
-    var = ['Item_Fat_Content', 'Outlet_Location_Type', 'Outlet_Type', 'Outlet_Size', 'Item_Type_Category']
+    lb=LabelEncoder()
+    data['Outlet']=lb.fit_transform(data['Outlet_Identifier'])
+    var=['Item_Fat_Content','Outlet_Location_Type','Outlet_Type','Outlet_Size','Item_Type_Category']
+    lb=LabelEncoder()
     for item in var:
-        data[item] = lb.fit_transform(data[item])
+        data[item]=lb.fit_transform(data[item])
 
-    data = pd.get_dummies(data, columns=['Item_Fat_Content', 'Outlet_Location_Type', 'Outlet_Type', 'Outlet_Size', 'Item_Type_Category'])
+    data = pd.get_dummies(data, columns=['Item_Fat_Content','Outlet_Location_Type','Outlet_Type','Outlet_Size','Item_Type_Category'])
 
     return data
 
 # Function to predict using the models
-def predict_sales(input_data):
-    # Load models and scaler
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    model_RNN = joblib.load(os.path.join(current_dir, 'model_RNN.pkl'))
-    model_LinReg = joblib.load(os.path.join(current_dir, 'model_LinReg.pkl'))
-    model_RidgeReg = joblib.load(os.path.join(current_dir, 'model_RidgeReg.pkl'))
-    scaler = joblib.load(os.path.join(current_dir, 'min_max_scaler.pkl'))
-
-    # Preprocess input data
-    input_data_processed = preprocess_input(input_data)
-
+def predict_sales(input_data, model_RNN, model_LinReg, model_RidgeReg, scaler):
     # Scale the input data
-    input_data_scaled = scaler.transform(input_data_processed)
+    input_data_scaled = scaler.transform(input_data)
 
     # Reshape for RNN model
     x_input = np.reshape(input_data_scaled, (input_data_scaled.shape[0], input_data_scaled.shape[1], 1))
@@ -77,6 +65,7 @@ def predict_sales(input_data):
 
     return predictions_RNN, predictions_LinReg, predictions_RidgeReg
 
+
 # Main function for Streamlit app
 def main():
     st.title("BigMart Sales Prediction Web App")
@@ -87,4 +76,21 @@ def main():
     user_input = {}
 
     user_input['Item_Identifier'] = st.sidebar.text_input("Item Identifier")
-    user_input['Item_Weight'] = st.sidebar.number_input
+    user_input['Item_Weight'] = st.sidebar.number_input("Item Weight", min_value=0.0, max_value=500.0)
+    user_input['Item_Visibility'] = st.sidebar.number_input("Item Visibility", min_value=0.0, max_value=1.0)
+    user_input['Item_MRP'] = st.sidebar.number_input("Item MRP", min_value=0.0)
+    user_input['Outlet_Identifier'] = st.sidebar.selectbox("Outlet Identifier", ["OUT010", "OUT013", "OUT017", "OUT018", "OUT019", "OUT027", "OUT035", "OUT045", "OUT046", "OUT049"])
+    user_input['Outlet_Size'] = st.sidebar.selectbox("Outlet Size", ["Small", "Medium", "High"])
+    user_input['Outlet_Location_Type'] = st.sidebar.selectbox("Outlet Location Type", ["Tier 1", "Tier 2", "Tier 3"])
+    user_input['Outlet_Type'] = st.sidebar.selectbox("Outlet Type", ["Supermarket Type1", "Supermarket Type2", "Supermarket Type3", "Grocery Store"])
+    user_input['Item_Fat_Content'] = st.sidebar.selectbox("Item Fat Content", ["Low Fat", "Regular", "Non-Edible"])
+    user_input['Item_Type'] = st.sidebar.selectbox("Item Type", ["Dairy", "Soft Drinks", "Meat", "Fruits and Vegetables", "Household", "Baking Goods", "Snack Foods", "Frozen Foods", "Breakfast", "Health and Hygiene", "Hard Drinks", "Canned", "Breads", "Starchy Foods", "Others", "Seafood"])
+
+    if st.sidebar.button("Predict"):
+        input_data = pd.DataFrame(user_input, index=[0])
+        input_data_processed = preprocess_input(input_data)
+
+        # Load models and scaler
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        model_RNN = joblib.load(os.path.join(current_dir, 'model_RNN.pkl'))
+        model_LinReg = joblib.load(os.path.join(current_dir, 'model_LinReg.pkl
